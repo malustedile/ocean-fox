0a0f604beb72c69f82c90f965f4f091dfc9573cc
malustedile <mstedile@alunos.utfpr.edu.br>
2025-04-09 22:32:38 -0300

    :sparkles: serviço de pagamento ponto de interrogação

--- pagamento/src/index.ts
+++ pagamento/src/index.ts
@@ -1,6 +1,81 @@
 import { Elysia } from "elysia";
+import amqp from "amqplib";
+import { readFileSync } from "fs";
+import { createSign } from "crypto";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+interface reservaPayload {
+  destino: string;
+  dataEmbarque: string;
+  numeroPassageiros: number;
+  numeroCabines: number;
+  linkPagamento: string;
+  status: string;
+  criadoEm: string;
+}
+
+const rabbit = await amqp.connect("amqp://localhost");
+const channelReserva = await rabbit.createChannel();
+const channelPagamentoAprovado = await rabbit.createChannel();
+const channelPagamentoRecusado = await rabbit.createChannel();
+
+await channelReserva.assertQueue("reserva-criada", { durable: true });
+await channelPagamentoAprovado.assertQueue("pagamento-aprovado", {
+  durable: true,
+});
+await channelPagamentoRecusado.assertQueue("pagamento-recusado", {
+  durable: true,
+});
+
+channelReserva.consume(
+  "reserva-criada",
+  (msg: any) => {
+    if (msg) {
+      const reserva = JSON.parse(msg.content.toString()) as reservaPayload;
+      console.log("Reserva recebida:", reserva);
+      channelReserva.ack(msg);
+
+      const pagamentoAprovado = Math.random() > 0.5;
+      reserva.status = pagamentoAprovado ? "aprovado" : "recusado";
+
+      const chavePrivada = readFileSync("./pagamento", "utf-8");
+      const signer = createSign("sha256");
+      signer.update(JSON.stringify(reserva));
+      signer.end();
+
+      const assinatura = signer.sign(
+        { key: chavePrivada, passphrase: "your-passphrase" },
+        "base64"
+      );
+
+      if (pagamentoAprovado) {
+        channelPagamentoAprovado.sendToQueue(
+          "pagamento-aprovado",
+          Buffer.from(
+            JSON.stringify({
+              reserva,
+              assinatura,
+            })
+          )
+        );
+        console.log("Pagamento aprovado:", reserva);
+      } else {
+        channelPagamentoRecusado.sendToQueue(
+          "pagamento-recusado",
+          Buffer.from(
+            JSON.stringify({
+              reserva,
+              assinatura,
+            })
+          )
+        );
+        console.log("Pagamento recusado:", reserva);
+      }
+    }
+  },
+  { noAck: false }
+);
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3001);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
