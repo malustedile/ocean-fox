a3fa68b909271a79d7a3bb4207953332529f3cc8
malustedile <mstedile@alunos.utfpr.edu.br>
2025-04-12 14:15:45 -0300

    :art: melhorias  navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,34 +3,44 @@ import { minhasReservas } from "../api/destinos";
 import { Screens } from "../App";
 
 interface NavBarProps {
+  activeScreen: Screens;
   setScreen: (screen: Screens) => any;
 }
 
-export const NavBar: React.FC<NavBarProps> = ({ setScreen }) => {
+export const NavBar: React.FC<NavBarProps> = ({ activeScreen, setScreen }) => {
   const handleMyReservationsClick = async () => {
     const res = await minhasReservas();
     console.log(res);
   };
 
+  const classNameButtons =
+    "hover:text-[#007090] hover:bg-[#cce2e9] hover:cursor-pointer px-4 py-1 rounded-full";
+
   return (
     <div className="flex items-center justify-center w-full mb-6 gap-2">
       <div
         onClick={() => setScreen(Screens.Itinerarios)}
-        className="hover:text-[#007090] hover:bg-[#cce2e9] hover:cursor-pointer px-4 py-1 rounded-full"
+        className={
+          activeScreen === Screens.Itinerarios
+            ? "bg-[#cce2e9] text-[#007090] hover:cursor-pointer px-4 py-1 rounded-full"
+            : classNameButtons
+        }
       >
         Itinerários
       </div>
       <div>•</div>
       <div
         onClick={() => setScreen(Screens.MinhasReservas)}
-        className="hover:text-[#007090] hover:bg-[#cce2e9] hover:cursor-pointer px-4 py-1 rounded-full"
+        className={
+          activeScreen === Screens.MinhasReservas
+            ? "bg-[#cce2e9] text-[#007090] hover:cursor-pointer px-4 py-1 rounded-full"
+            : classNameButtons
+        }
       >
         Minhas Reservas
       </div>
       <div>•</div>
-      <div className="hover:text-[#007090] hover:bg-[#cce2e9] hover:cursor-pointer px-4 py-1 rounded-full">
-        Inscreva-se
-      </div>
+      <div className={classNameButtons}>Inscreva-se</div>
     </div>
   );
 };
